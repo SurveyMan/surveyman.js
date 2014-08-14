03681d5d9736ee313087c09cd0001e967f794db8
etosch <etosch@gmail.com>
2014-08-13 22:24:32 -0400

    trying to trigger a travis build

--- tests/surveyman_tests.js
+++ tests/surveyman_tests.js
@@ -10,5 +10,10 @@ for (var i = 0 ; i < jsonizedSurveys.length ; i++) {
     var survey = SurveyMan.survey.init(jsonizedSurveys[1]);
     if (s(survey.filename).endsWith("prototypicality.csv")){
         console.assert(_.all(_.pick(survey.topLevelBlocks, "randomizable")));
+        for (var j = 0 ; j < survey.topLevelBlocks.length ; j++ ){
+            if (survey.topLevelBlocks[j].length===4)
+                console.assert(survey.topLevelBlocks[j].isBranchAll());
+            else console.assert(survey.topLevelBlocks.length===2);
+        }
     }
 }
