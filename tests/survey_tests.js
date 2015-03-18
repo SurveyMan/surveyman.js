b41370c3c268f028c1d5f1a659c56f30508961e5
etosch <etosch@gmail.com>
2015-03-17 20:50:36 -0400

    survey_tests.js pass when running from inside node, but they are currently failing when running npm test

--- tests/survey_tests.js
+++ tests/survey_tests.js
@@ -7,72 +7,74 @@ var vows = require("vows"),
 require("SurveyMan/survey.js");
 
 
-var network_test = function () {
+var network_test = function (callback) {
     var s = require("string"),
         _ = require("underscore"),
-        $ = require("jquery"),
-        http = require("http"),
+        request = require("request"),
         jjv = require("jjv"),
         env = jjv(),
         statusCode = null,
-        options = {
-                hostname : "surveyman.github.io",
-                port : 80,
-                path : "/Schemata/survey_output.json",
-                method : "GET"
+        schemata = {
+            'survey_output' : null,
+            'survey_block' : null,
+            'survey_question' : null,
+            'survey_branchMap' : null,
+            'survey_option' : null
         },
-        schema = "",
-        retval = null,
-        cbk = function(response) {
-                statusCode = response.statusCode;
-                response.setEncoding("UTF-8");
-
-                response.on("data", function (d){
-                    schema += d.toString();
+        make_request = function (schema_name, cbk) {
+            var schema = "";
+            request("http://surveyman.github.io/Schemata/" + schema_name + ".json",
+                function (error, response, body) {
+                    console.log("schema_name", schema_name);
+                    if (!error && response.statusCode === 200) {
+                        schemata[schema_name]  = JSON.parse(body);
+                    } else {
+                        console.log("*****" + error + "*****");
+                    }
+                    cbk();
                 });
+        };
 
-                response.on("end", function () {
-                    env.addSchema('output_survey', JSON.parse(schema));
-                    var result1 = env.validate('output_survey', globals['wage_survey']),
-                        result2 = env.validate('output_survey', globals['prototypicality_survey']),
-                        result3 = env.validate('output_survey', globals['pick_randomly_survey']),
-                        result4 = env.validate('output_survey', {
-                            filename : "foo",
-                            breakoff : false,
-                            survey : true }),
-                        result5 = env.validate('output_survey', {
-                            filename: "foo",
-                            breakoff: false,
-                            survey : [{id : 1}]});
-                    console.log("result1: " + JSON.stringify(result1));
-                    console.log("result2: " + JSON.stringify(result2));
-                    console.log("result3: " + JSON.stringify(result3));
-                    console.log("result4: " + JSON.stringify(result4));
-                    console.log("result5: " + JSON.stringify(result5));
-                });
-        },
-        req = http.request(options, cbk);
+    var stem = "http://surveyman.github.io/Schemata/",
+        survey_output = stem+"survey_output.json";
 
     env.defaultOptions.checkRequired = true;
 
-    req.on("error", function (e){
-        console.log("***",e,"***");
-    });
-
-    req.end();
-
-    console.log("status code: " + statusCode  + "\nretval: " + retval);
-
-    return statusCode === 200;
+    _.foldl(_.keys(schemata),
+        function(memo, num) { return function () { make_request(num, memo);  }},
+        function() {
+            _.map(_.keys(schemata), function (schema_name) {
+                var keyname = stem+schema_name+".json";
+               console.log(keyname);
+               env.addSchema(keyname, schemata[schema_name]);
+            });
+            var result1 = env.validate(survey_output, globals['wage_survey']),
+                result2 = env.validate(survey_output, globals['prototypicality_survey']),
+                result3 = env.validate(survey_output, globals['pick_randomly_survey']),
+                result4 = env.validate(survey_output, {
+                    filename : "foo",
+                    breakoff : false,
+                    survey : true }),
+                result5 = env.validate(survey_output, {
+                    filename: "foo",
+                    breakoff: false,
+                    survey : [{asdf : 1}]});
+            console.log("result1: " + JSON.stringify(result1));
+            console.log("result2: " + JSON.stringify(result2));
+            console.log("result3: " + JSON.stringify(result3));
+            console.log("result4: " + JSON.stringify(result4));
+            console.log("result5: " + JSON.stringify(result5));
+            assert(!(result1 || result2 || result3) && result4 && result5);
+            if (callback)
+                callback();
+        })();
 };
 
 vows.describe("Check validation").addBatch({
     "Validation doesn't cause errors" : {
         topic : 'Network Test',
         "Srsly doesn't fail." : function (_) {
-            var outcome = network_test();
-            console.log(outcome);
-            assert.ok(outcome);
+            return network_test();
         }
     }
 }).run();
