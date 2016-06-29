003793d9e504b0596ad8378699ee92fcb30b09bb
etosch <etosch@gmail.com>
2016-06-29 17:13:00 -0400

    deleted exclude from wepack foo (was excluding from bundling)

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ module.exports = {
     module : {
         loaders : [
             {
-                exclude : /(node_modules)/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015']
